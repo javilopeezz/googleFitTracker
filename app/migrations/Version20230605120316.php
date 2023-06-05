b0e4d24206b79927d96f60b6bda3f6e4c499c26d
Javier López <javier17lg@gmail.com>
2023-06-05 17:15:40 +0200

    Comienzo User

--- app/migrations/Version20230605120316.php
+++ app/migrations/Version20230605120316.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230522154337 extends AbstractMigration
+final class Version20230605120316 extends AbstractMigration
 {
     public function getDescription(): string
     {
