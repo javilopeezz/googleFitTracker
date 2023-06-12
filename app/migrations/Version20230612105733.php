d95d55cd796f9a25cb855d1ad43f30c46ea23c79
Pablo1603-12 <pablomarquinezjimenez3@gmail.com>
2023-06-12 13:06:02 +0200

    Migracion corregida y README actualizado

--- app/migrations/Version20230612105733.php
+++ app/migrations/Version20230612105733.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230605120316 extends AbstractMigration
+final class Version20230612105733 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20230605120316 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE datos (id INT AUTO_INCREMENT NOT NULL, email_usuario VARCHAR(255) NOT NULL, pasos INT NOT NULL, distancia DOUBLE PRECISION NOT NULL, peso DOUBLE PRECISION NOT NULL, calorias_quemadas DOUBLE PRECISION NOT NULL, fecha DATE NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE usuario (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_2265B05DF85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE datos');
+        $this->addSql('DROP TABLE usuario');
     }
 }
